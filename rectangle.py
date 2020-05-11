91e642bae13dad83287fdede2b8ce5aae9ad5ae7
khanh196200 <62270609+khanh196200@users.noreply.github.com>
2020-05-11 21:32:21 +0700

    Add files via upload

---
+++ rectangle.py
@@ -0,0 +1,10 @@
+class Hinhchunhat(object):
+ #### set values
+   def __init__(self, a, b):
+      self.a = a
+      self.b = b
+ ####
+   def area(self):
+      return self.a*self.b
+aHinhchunhat = Hinhchunhat(a,b)
+print (aHinhchunhat.area())
